slothdabski/android_device_oneplus_sm8150

sm8150-common: DeviceSettings: Send broadcast on alert slider change
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

DeviceSettings/src/org/evolution/device/DeviceSettings/Constants.java
@@ -52,6 +52,10 @@ public class Constants {
     public static final Map<Integer, String> sKeyMap = new HashMap<>();
     public static final Map<String, Integer> sKeyDefaultMap = new HashMap<>();
 
+    public static final String ACTION_UPDATE_SLIDER_POSITION
+            = "org.evolution.device.DeviceSettings.UPDATE_SLIDER_POSITION";
+    public static final String EXTRA_SLIDER_POSITION = "position";
+
     static {
         sStringKeyPreferenceMap.put(NOTIF_SLIDER_TOP_KEY, NOTIF_SLIDER_TOP_PREF);
         sStringKeyPreferenceMap.put(NOTIF_SLIDER_MIDDLE_KEY, NOTIF_SLIDER_MIDDLE_PREF);

BootleggersROM-Devices/android_device_oneplus_sm8150-common

Signed-off-by: AnierinB <anierinb@evolution-x.org>

DeviceSettings/src/com/bootleggers/device/DeviceSettings/Constants.java
@@ -52,6 +52,10 @@ public class Constants {
     public static final Map<Integer, String> sKeyMap = new HashMap<>();
     public static final Map<String, Integer> sKeyDefaultMap = new HashMap<>();
 
+    public static final String ACTION_UPDATE_SLIDER_POSITION
+            = "com.bootleggers.device.DeviceSettings.UPDATE_SLIDER_POSITION";
+    public static final String EXTRA_SLIDER_POSITION = "position";
+
     static {
         sStringKeyPreferenceMap.put(NOTIF_SLIDER_TOP_KEY, NOTIF_SLIDER_TOP_PREF);
         sStringKeyPreferenceMap.put(NOTIF_SLIDER_MIDDLE_KEY, NOTIF_SLIDER_MIDDLE_PREF);