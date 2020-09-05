ShadowOS-Devices/device_oneplus_sm8150-common

sm8150-common: DeviceSettings: Add SELinux Switch
Reference: https://github.com/DerpLab/platform_packages_apps_DerpQuest/commit/65f48a63a8e43ca041462a9c2280435ad15779a6

Vectors From: https://materialdesignicons.com

EvoX edits:
- Drop BootCompletedReceiver for startup.java as it's essentially duplicate code. Despite that, thermal profiles weren't even starting on boot.
- Disable selinux preferences if su is not found.

Co-authored-by: AnierinB <anierinb@evolution-x.org>
signed-off-by: anierinb <anierinb@evolution-x.org>
Signed-off-by: Justin Crawford <Justin@stacksmash.net>

DeviceSettings/src/org/evolution/device/DeviceSettings/DeviceSettings.java
@@ -26,6 +26,7 @@ import android.content.res.Resources;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SELinux;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.MenuItem;
@@ -49,6 +50,9 @@ import androidx.preference.TwoStatePreference;
 
 import org.evolution.device.DeviceSettings.FileUtils;
 import org.evolution.device.DeviceSettings.Constants;
+import org.evolution.device.DeviceSettings.R;
+import org.evolution.device.DeviceSettings.SuShell;
+import org.evolution.device.DeviceSettings.SuTask;
 
 public class DeviceSettings extends PreferenceFragment
         implements Preference.OnPreferenceChangeListener {
@@ -71,9 +75,12 @@ public class DeviceSettings extends PreferenceFragment
 
     public static final String KEY_VIBSTRENGTH = "vib_strength";
 
-
     public static final String KEY_SETTINGS_PREFIX = "device_setting_";
 
+    private static final String SELINUX_CATEGORY = "selinux";
+    private static final String PREF_SELINUX_MODE = "selinux_mode";
+    private static final String PREF_SELINUX_PERSISTENCE = "selinux_persistence";
+    
     private static TwoStatePreference mEnableDolbyAtmos;
     private static TwoStatePreference mHBMModeSwitch;
     private static TwoStatePreference mAutoHBMSwitch;
@@ -85,6 +92,8 @@ public class DeviceSettings extends PreferenceFragment
     private ListPreference mMiddleKeyPref;
     private ListPreference mBottomKeyPref;
     private VibratorStrengthPreference mVibratorStrength;
+    private SwitchPreference mSelinuxMode;
+    private SwitchPreference mSelinuxPersistence;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -150,6 +159,21 @@ public class DeviceSettings extends PreferenceFragment
             getPreferenceScreen().removePreference((Preference) findPreference(KEY_CATEGORY_REFRESH));
         }
 
+        // SELinux
+        boolean isRooted = SuShell.detectValidSuInPath();
+        Preference selinuxCategory = findPreference(SELINUX_CATEGORY);
+        mSelinuxMode = (SwitchPreference) findPreference(PREF_SELINUX_MODE);
+        mSelinuxMode.setChecked(SELinux.isSELinuxEnforced());
+        mSelinuxMode.setOnPreferenceChangeListener(this);
+        mSelinuxMode.setEnabled(isRooted);
+
+        mSelinuxPersistence =
+        (SwitchPreference) findPreference(PREF_SELINUX_PERSISTENCE);
+        mSelinuxPersistence.setOnPreferenceChangeListener(this);
+        mSelinuxPersistence.setChecked(getContext()
+        .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE)
+        .contains(PREF_SELINUX_MODE));
+        mSelinuxPersistence.setEnabled(isRooted);
     }
 
     @Override
@@ -175,8 +199,13 @@ public class DeviceSettings extends PreferenceFragment
             SharedPreferences.Editor prefChange = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
             prefChange.putBoolean(KEY_AUTO_HBM_SWITCH, enabled).commit();
             Utils.enableService(getContext());
-        }
-        else if (preference == mEnableDolbyAtmos) {
+        } else if (preference == mSelinuxMode) {
+            boolean enabled = (Boolean) newValue;
+            new SwitchSelinuxTask(getActivity()).execute(enabled);
+            setSelinuxEnabled(enabled, mSelinuxPersistence.isChecked());
+        } else if (preference == mSelinuxPersistence) {
+            setSelinuxEnabled(mSelinuxMode.isChecked(), (Boolean) newValue);
+        } else if (preference == mEnableDolbyAtmos) {
             boolean enabled = (Boolean) newValue;
             Intent daxService = new Intent();
             ComponentName name = new ComponentName("com.dolby.daxservice", "com.dolby.daxservice.DaxService");
@@ -192,11 +221,9 @@ public class DeviceSettings extends PreferenceFragment
                 this.getContext().getPackageManager().setComponentEnabledSetting(name,
                         PackageManager.COMPONENT_ENABLED_STATE_DISABLED, 0);
             }
-            return true; 
         }
         else {
             Constants.setPreferenceInt(getContext(), preference.getKey(), Integer.parseInt((String) newValue));
-            return true;
         } 
         return true;
     }
@@ -214,5 +241,43 @@ public class DeviceSettings extends PreferenceFragment
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setSelinuxEnabled(boolean status, boolean persistent) {
+      SharedPreferences.Editor editor = getContext()
+          .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE).edit();
+      if (persistent) {
+        editor.putBoolean(PREF_SELINUX_MODE, status);
+      } else {
+        editor.remove(PREF_SELINUX_MODE);
+      }
+      editor.apply();
+      mSelinuxMode.setChecked(status);
+    }
+
+    private class SwitchSelinuxTask extends SuTask<Boolean> {
+      public SwitchSelinuxTask(Context context) {
+        super(context);
+      }
+      @Override
+      protected void sudoInBackground(Boolean... params) throws SuShell.SuDeniedException {
+        if (params.length != 1) {
+          return;
+        }
+        if (params[0]) {
+          SuShell.runWithSuCheck("setenforce 1");
+        } else {
+          SuShell.runWithSuCheck("setenforce 0");
+        }
+      }
+
+      @Override
+      protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        if (!result) {
+          // Did not work, so restore actual value
+          setSelinuxEnabled(SELinux.isSELinuxEnforced(), mSelinuxPersistence.isChecked());
+        }
+      }
     }
 }

DeviceSettings/src/org/evolution/device/DeviceSettings/SuShell.java
@@ -1,0 +1,164 @@
+package org.evolution.device.DeviceSettings;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.util.Log;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SuShell {
+
+    private static final String TAG = SuShell.class.getSimpleName();
+
+    // WARNING: setting this to true will dump passwords to logcat
+    // set to false for release
+    static final boolean DEBUG = false;
+
+    private SuShell() {
+    }
+
+    public static ArrayList<String> runWithSuCheck(String... commands) throws SuDeniedException {
+        String suTestScript = "#!/system/bin/sh\necho ";
+        String suTestScriptValid = "PartsSuPermsOk";
+
+        String[] commandsWithCheck = new String[commands.length+1];
+        commandsWithCheck[0] = suTestScript + suTestScriptValid;
+        System.arraycopy(commands, 0, commandsWithCheck, 1, commands.length);
+
+        ArrayList<String> output = runWithSu(commandsWithCheck);
+        if (output.size() >= 1
+                && output.get(0).trim().equals(suTestScriptValid)) {
+            if (DEBUG) {
+                Log.d(TAG, "Superuser command auth confirmed");
+            }
+            output.remove(0);
+            return output;
+        } else {
+            if (DEBUG) {
+                Log.d(TAG, "Superuser command auth refused");
+            }
+            throw new SuDeniedException();
+        }
+    }
+
+    public static ArrayList<String> runWithShell(String... command) {
+        return run("/system/bin/sh", command);
+    }
+
+    public static ArrayList<String> runWithSu(String... command) {
+        return run("su", command);
+    }
+
+    public static ArrayList<String> run(String shell, ArrayList<String> commands) {
+        String[] commandsArray = new String[commands.size()];
+        commands.toArray(commandsArray);
+        return run(shell, commandsArray);
+    }
+
+    public static ArrayList<String> run(String shell, String... commands) {
+        ArrayList<String> output = new ArrayList<String>();
+
+        try {
+            Process process = Runtime.getRuntime().exec(shell);
+
+            BufferedOutputStream shellInput = new BufferedOutputStream(
+                    process.getOutputStream());
+            BufferedReader shellOutput = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+
+            for (String command : commands) {
+                if (DEBUG) {
+                    Log.i(TAG, "command: " + command);
+                }
+                shellInput.write((command + " 2>&1\n").getBytes());
+            }
+
+            shellInput.write("exit\n".getBytes());
+            shellInput.flush();
+
+            String line;
+            while ((line = shellOutput.readLine()) != null) {
+                if (DEBUG) {
+                    Log.d(TAG, "command output: " + line);
+                }
+                output.add(line);
+            }
+
+            process.waitFor();
+        } catch (IOException e) {
+            Log.e(TAG, "Error: " + e.getMessage(), e);
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            Log.e(TAG, "Error: " + e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+
+        return output;
+    }
+
+    public static String getCommandOutput(String command)
+            throws IOException {
+
+        StringBuilder output = new StringBuilder();
+
+        if (DEBUG) {
+            Log.d(TAG, "Getting output for command: " + command);
+        }
+        Process p = Runtime.getRuntime().exec(command);
+        InputStream is = p.getInputStream();
+        InputStreamReader r = new InputStreamReader(is);
+        BufferedReader in = new BufferedReader(r);
+
+        String line;
+        while ((line = in.readLine()) != null) {
+            output.append(line);
+            output.append("\n");
+        }
+
+        return output.toString();
+    }
+
+    public static boolean detectValidSuInPath() {
+        String[] pathToTest = System.getenv("PATH").split(":");
+
+        for (String path : pathToTest) {
+            File su = new File(path + "/su");
+            if (su.exists()) {
+                // should check if it is suid
+                if (DEBUG) {
+                    Log.d(TAG, "Found su at " + su.getAbsolutePath());
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean findInPath(String cmd) {
+        String[] pathToTest = System.getenv("PATH").split(":");
+
+        for (String path : pathToTest) {
+            File cmdFile = new File(path, cmd);
+            if (cmdFile.exists()) {
+                if (DEBUG) {
+                    Log.d(TAG, "Found " + cmd + " at " + cmdFile.getAbsolutePath());
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static class SuDeniedException extends Exception {};
+}

DeviceSettings/src/org/evolution/device/DeviceSettings/SuTask.java
@@ -1,0 +1,52 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evolution.device.DeviceSettings;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+public abstract class SuTask<Params> extends AsyncTask<Params, Void, Boolean> {
+    private Context mContext;
+
+    public SuTask(Context context) {
+        super();
+        mContext = context;
+    }
+
+    abstract protected void sudoInBackground(Params... params)
+            throws SuShell.SuDeniedException;
+
+    @Override
+    protected Boolean doInBackground(Params... params) {
+        try {
+            sudoInBackground(params);
+            return true;
+        } catch (SuShell.SuDeniedException e) {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        if (!result) {
+            Toast.makeText(mContext, mContext.getString(R.string.cannot_get_su),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}

BootleggersROM-Devices/android_device_oneplus_sm8150-common

DeviceSettings/src/com/bootleggers/device/DeviceSettings/DeviceSettings.java
@@ -26,6 +26,7 @@ import android.content.res.Resources;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SELinux;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.MenuItem;
@@ -47,6 +48,9 @@ import androidx.preference.TwoStatePreference;
 
 import com.bootleggers.device.DeviceSettings.FileUtils;
 import com.bootleggers.device.DeviceSettings.Constants;
+import com.bootleggers.device.DeviceSettings.R;
+import com.bootleggers.device.DeviceSettings.SuShell;
+import com.bootleggers.device.DeviceSettings.SuTask;
 
 public class DeviceSettings extends PreferenceFragment
         implements Preference.OnPreferenceChangeListener {
@@ -69,9 +73,12 @@ public class DeviceSettings extends PreferenceFragment
 
     public static final String KEY_VIBSTRENGTH = "vib_strength";
 
-
     public static final String KEY_SETTINGS_PREFIX = "device_setting_";
 
+    private static final String SELINUX_CATEGORY = "selinux";
+    private static final String PREF_SELINUX_MODE = "selinux_mode";
+    private static final String PREF_SELINUX_PERSISTENCE = "selinux_persistence";
+    
     private static TwoStatePreference mEnableDolbyAtmos;
     private static TwoStatePreference mHBMModeSwitch;
     private static TwoStatePreference mAutoHBMSwitch;
@@ -83,6 +90,8 @@ public class DeviceSettings extends PreferenceFragment
     private ListPreference mMiddleKeyPref;
     private ListPreference mBottomKeyPref;
     private VibratorStrengthPreference mVibratorStrength;
+    private SwitchPreference mSelinuxMode;
+    private SwitchPreference mSelinuxPersistence;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -139,6 +148,21 @@ public class DeviceSettings extends PreferenceFragment
             getPreferenceScreen().removePreference((Preference) findPreference(KEY_CATEGORY_REFRESH));
         }
 
+        // SELinux
+        boolean isRooted = SuShell.detectValidSuInPath();
+        Preference selinuxCategory = findPreference(SELINUX_CATEGORY);
+        mSelinuxMode = (SwitchPreference) findPreference(PREF_SELINUX_MODE);
+        mSelinuxMode.setChecked(SELinux.isSELinuxEnforced());
+        mSelinuxMode.setOnPreferenceChangeListener(this);
+        mSelinuxMode.setEnabled(isRooted);
+
+        mSelinuxPersistence =
+        (SwitchPreference) findPreference(PREF_SELINUX_PERSISTENCE);
+        mSelinuxPersistence.setOnPreferenceChangeListener(this);
+        mSelinuxPersistence.setChecked(getContext()
+        .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE)
+        .contains(PREF_SELINUX_MODE));
+        mSelinuxPersistence.setEnabled(isRooted);
     }
 
     @Override
@@ -164,8 +188,13 @@ public class DeviceSettings extends PreferenceFragment
             SharedPreferences.Editor prefChange = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
             prefChange.putBoolean(KEY_AUTO_HBM_SWITCH, enabled).commit();
             Utils.enableService(getContext());
-        }
-        else if (preference == mEnableDolbyAtmos) {
+        } else if (preference == mSelinuxMode) {
+            boolean enabled = (Boolean) newValue;
+            new SwitchSelinuxTask(getActivity()).execute(enabled);
+            setSelinuxEnabled(enabled, mSelinuxPersistence.isChecked());
+        } else if (preference == mSelinuxPersistence) {
+            setSelinuxEnabled(mSelinuxMode.isChecked(), (Boolean) newValue);
+        } else if (preference == mEnableDolbyAtmos) {
             boolean enabled = (Boolean) newValue;
             Intent daxService = new Intent();
             ComponentName name = new ComponentName("com.dolby.daxservice", "com.dolby.daxservice.DaxService");
@@ -181,11 +210,9 @@ public class DeviceSettings extends PreferenceFragment
                 this.getContext().getPackageManager().setComponentEnabledSetting(name,
                         PackageManager.COMPONENT_ENABLED_STATE_DISABLED, 0);
             }
-            return true; 
         }
         else {
             Constants.setPreferenceInt(getContext(), preference.getKey(), Integer.parseInt((String) newValue));
-            return true;
         } 
         return true;
     }
@@ -203,5 +230,43 @@ public class DeviceSettings extends PreferenceFragment
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setSelinuxEnabled(boolean status, boolean persistent) {
+      SharedPreferences.Editor editor = getContext()
+          .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE).edit();
+      if (persistent) {
+        editor.putBoolean(PREF_SELINUX_MODE, status);
+      } else {
+        editor.remove(PREF_SELINUX_MODE);
+      }
+      editor.apply();
+      mSelinuxMode.setChecked(status);
+    }
+
+    private class SwitchSelinuxTask extends SuTask<Boolean> {
+      public SwitchSelinuxTask(Context context) {
+        super(context);
+      }
+      @Override
+      protected void sudoInBackground(Boolean... params) throws SuShell.SuDeniedException {
+        if (params.length != 1) {
+          return;
+        }
+        if (params[0]) {
+          SuShell.runWithSuCheck("setenforce 1");
+        } else {
+          SuShell.runWithSuCheck("setenforce 0");
+        }
+      }
+
+      @Override
+      protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        if (!result) {
+          // Did not work, so restore actual value
+          setSelinuxEnabled(SELinux.isSELinuxEnforced(), mSelinuxPersistence.isChecked());
+        }
+      }
     }
 }

DeviceSettings/src/com/bootleggers/device/DeviceSettings/SuShell.java
@@ -1,0 +1,164 @@
+package com.bootleggers.device.DeviceSettings;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.util.Log;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SuShell {
+
+    private static final String TAG = SuShell.class.getSimpleName();
+
+    // WARNING: setting this to true will dump passwords to logcat
+    // set to false for release
+    static final boolean DEBUG = false;
+
+    private SuShell() {
+    }
+
+    public static ArrayList<String> runWithSuCheck(String... commands) throws SuDeniedException {
+        String suTestScript = "#!/system/bin/sh\necho ";
+        String suTestScriptValid = "PartsSuPermsOk";
+
+        String[] commandsWithCheck = new String[commands.length+1];
+        commandsWithCheck[0] = suTestScript + suTestScriptValid;
+        System.arraycopy(commands, 0, commandsWithCheck, 1, commands.length);
+
+        ArrayList<String> output = runWithSu(commandsWithCheck);
+        if (output.size() >= 1
+                && output.get(0).trim().equals(suTestScriptValid)) {
+            if (DEBUG) {
+                Log.d(TAG, "Superuser command auth confirmed");
+            }
+            output.remove(0);
+            return output;
+        } else {
+            if (DEBUG) {
+                Log.d(TAG, "Superuser command auth refused");
+            }
+            throw new SuDeniedException();
+        }
+    }
+
+    public static ArrayList<String> runWithShell(String... command) {
+        return run("/system/bin/sh", command);
+    }
+
+    public static ArrayList<String> runWithSu(String... command) {
+        return run("su", command);
+    }
+
+    public static ArrayList<String> run(String shell, ArrayList<String> commands) {
+        String[] commandsArray = new String[commands.size()];
+        commands.toArray(commandsArray);
+        return run(shell, commandsArray);
+    }
+
+    public static ArrayList<String> run(String shell, String... commands) {
+        ArrayList<String> output = new ArrayList<String>();
+
+        try {
+            Process process = Runtime.getRuntime().exec(shell);
+
+            BufferedOutputStream shellInput = new BufferedOutputStream(
+                    process.getOutputStream());
+            BufferedReader shellOutput = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+
+            for (String command : commands) {
+                if (DEBUG) {
+                    Log.i(TAG, "command: " + command);
+                }
+                shellInput.write((command + " 2>&1\n").getBytes());
+            }
+
+            shellInput.write("exit\n".getBytes());
+            shellInput.flush();
+
+            String line;
+            while ((line = shellOutput.readLine()) != null) {
+                if (DEBUG) {
+                    Log.d(TAG, "command output: " + line);
+                }
+                output.add(line);
+            }
+
+            process.waitFor();
+        } catch (IOException e) {
+            Log.e(TAG, "Error: " + e.getMessage(), e);
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            Log.e(TAG, "Error: " + e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+
+        return output;
+    }
+
+    public static String getCommandOutput(String command)
+            throws IOException {
+
+        StringBuilder output = new StringBuilder();
+
+        if (DEBUG) {
+            Log.d(TAG, "Getting output for command: " + command);
+        }
+        Process p = Runtime.getRuntime().exec(command);
+        InputStream is = p.getInputStream();
+        InputStreamReader r = new InputStreamReader(is);
+        BufferedReader in = new BufferedReader(r);
+
+        String line;
+        while ((line = in.readLine()) != null) {
+            output.append(line);
+            output.append("\n");
+        }
+
+        return output.toString();
+    }
+
+    public static boolean detectValidSuInPath() {
+        String[] pathToTest = System.getenv("PATH").split(":");
+
+        for (String path : pathToTest) {
+            File su = new File(path + "/su");
+            if (su.exists()) {
+                // should check if it is suid
+                if (DEBUG) {
+                    Log.d(TAG, "Found su at " + su.getAbsolutePath());
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean findInPath(String cmd) {
+        String[] pathToTest = System.getenv("PATH").split(":");
+
+        for (String path : pathToTest) {
+            File cmdFile = new File(path, cmd);
+            if (cmdFile.exists()) {
+                if (DEBUG) {
+                    Log.d(TAG, "Found " + cmd + " at " + cmdFile.getAbsolutePath());
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static class SuDeniedException extends Exception {};
+}

DeviceSettings/src/com/bootleggers/device/DeviceSettings/SuTask.java
@@ -1,0 +1,52 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bootleggers.device.DeviceSettings;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+public abstract class SuTask<Params> extends AsyncTask<Params, Void, Boolean> {
+    private Context mContext;
+
+    public SuTask(Context context) {
+        super();
+        mContext = context;
+    }
+
+    abstract protected void sudoInBackground(Params... params)
+            throws SuShell.SuDeniedException;
+
+    @Override
+    protected Boolean doInBackground(Params... params) {
+        try {
+            sudoInBackground(params);
+            return true;
+        } catch (SuShell.SuDeniedException e) {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        if (!result) {
+            Toast.makeText(mContext, mContext.getString(R.string.cannot_get_su),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}